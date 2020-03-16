678ce6daf060fafc90e08b71fc0ad53dd9b68b22
mops1k <bednyj.mops@gmail.com>
2020-03-16 16:58:46 +0300

    add active for menu

--- src/Form/SettingsType.php
+++ src/Form/SettingsType.php
@@ -6,6 +6,7 @@ use App\DataTransfer\SettingsDTO;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,7 @@ class SettingsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('serverName')
+            ->add('serverName', TextType::class, ['required' => true])
             ->add('adminPassword', PasswordType::class, [
                 'always_empty' => false,
             ])
